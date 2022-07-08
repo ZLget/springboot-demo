c58f44d85bc6c234af5d1db54a7ce2d903f020c0
julianZhu <julianzhu@can-dao.com>
2022-07-08 15:43:10 +0800

    初始化

---
+++ demo2/src/test/java/com/jenkins/demo/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.demo.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
